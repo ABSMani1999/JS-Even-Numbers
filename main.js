274af6c8303887cd3953532a6a944c84d4dd39d0
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-25 07:23:56 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,10 @@
+//even number		
+var n=[12,24,23,52,79,15,46];
+
+	for (a=0;a<n.length;a++) {
+		if (n[a]%2==0) {
+			document.write(n[a]);
+		} 
+ } 
+ document.write("<br>");
+ document.write("<br>");
